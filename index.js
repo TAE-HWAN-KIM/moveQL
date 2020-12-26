81598ab830bb11198d2887bfa788437d0d9f54c5
TAE-HWAN-KIM <shsrgg@gmail.com>
2020-12-26 16:58:05 +0900

    commit

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 import { GraphQLServer } from "graphql-yoga";
-import testresolvers from "./graphql/testresolvers";
-// import resolvers from "./graphql/resolvers";
-
-//const server = new GraphQLServer({
-//   typeDefs: `type Movie {
-//     id: Int!
-//     title: String!
-//     rating: Float
-//     description_intro: String
-//     language: String
-//     medium_cover_image: String
-//     genres: [String]
-//  }
-  
-//   type Query {
-//     movies(limit: Int, rating: Float): [Movie]!
-//     movie(id: Int!): Movie
-//     suggestions(id: Int!): [Movie]!
-//   }
-//   `,
-//   resolvers
-//});
+//import testresolvers from "./graphql/testresolvers";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-       typeDefs: './graphql/test.graphql',
-       resolvers : testresolvers
-    });
+  typeDefs: [
+    `type Movie {
+        id: Int!
+        title: String!
+        rating: Float
+        description_intro: String
+        language: String
+        medium_cover_image: String
+        genres: [String]
+     }
+      
+      type Query {
+        movies(limit: Int, rating: Float): [Movie]!
+        movie(id: Int!): Movie
+        suggestions(id: Int!): [Movie]!
+      }
+      `
+  ],
+  resolvers : resolvers
+});
+
+// const server = new GraphQLServer({
+//        typeDefs: './graphql/test.graphql',
+//        resolvers : testresolvers
+//     });
 
 server.start(() => console.log("Graphql Server Running"));
